bd53d4c5043990abac61717610e240bdd6d08928
EJr <EJr@pc-EJr>
2015-11-10 22:06:15 -0200

    teste1

---
+++ teste/src/com/teste/teste1.java
@@ -0,0 +1,19 @@
+package com.teste;
+
+public class teste1 {
+
+	public static void main(String[] args) {
+		boolean teste;
+		
+		teste = true;
+		
+		if(teste==true){
+			System.out.println("Teste Verdadeiro");
+		}
+		else{
+			System.out.println("Teste Falso");
+		}
+
+	}
+
+}
